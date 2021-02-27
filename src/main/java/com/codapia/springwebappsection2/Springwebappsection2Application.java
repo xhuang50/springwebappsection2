06c8fb7dc8d573f133ae71756dba697017cabc7a
Xin Huang <xhuang0314@gmail.com>
2021-02-27 17:49:05 -0600

    Initial Commit

---
+++ src/main/java/com/codapia/springwebappsection2/Springwebappsection2Application.java
@@ -0,0 +1,13 @@
+package com.codapia.springwebappsection2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebappsection2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springwebappsection2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codapia/springwebappsection2/Springwebappsection2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codapia.springwebappsection2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springwebappsection2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
